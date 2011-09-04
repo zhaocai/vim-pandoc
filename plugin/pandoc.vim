zhaocai/vim-pandoc

commit 5e4c4c616889d24ad54ec6910c78fc4b6d89a8f3

Allow for no value of g:pandoc_bibfile in commands to execute.
Made special forms of conversion commands that used --bibliography default. If g:pandoc_bibfile is not set, the --bibliography arg is omitted.

plugin/pandoc.vim
@@ -141,8 +141,17 @@ def pandoc_execute(command, open_when_done=True):
 	# pass the value of our variables (e.g, g:pandoc_bibfile).
 	for value in command:
 		if value.startswith("g:"):
-			command[command.index(value)] = vim.eval(value)
-	
+			vim_value = vim.eval(value)
+			if vim_value == "":
+				if command[command.index(value) - 1] == "--bibliography":
+					command.remove(command[command.index(value) - 1])
+					command.remove(value)
+				else:
+					command[command.index(value)] = vim_value
+			else:
+				command[command.index(value)] = vim_value
+
+
 	# we run pandoc with our arguments
 	output = Popen(command, stdout=PIPE, stderr=PIPE).communicate()
 
@@ -206,15 +215,10 @@ command! -nargs=? PandocRegisterExecutor exec 'py pandoc_register_executor("<arg
 "
 " Generate html and open in default html viewer
 PandocRegisterExecutor PandocHtmlOpen <LocalLeader>html 1 pandoc -t html -Ss
-" Generate pdf and open in default pdf viewer
-PandocRegisterExecutor PandocPdfOpen <LocalLeader>pdf 1 markdown2pdf
-" Generate odt and open in default odt viewer
-PandocRegisterExecutor PandocOdtOpen <LocalLeader>odt 1 pandoc -t odt
-" Generate pdf w/ citeproc and open in default pdf view
-PandocRegisterExecutor PandocPdfBibOpen <LocalLeader>pdfb 1 markdown2pdf --bibliography g:pandoc_bibfile
+" Generate pdf w/ citeproc and open in default pdf viewer
+PandocRegisterExecutor PandocPdfOpen <LocalLeader>pdf 1 markdown2pdf --bibliography g:pandoc_bibfile
 " Generate odt w/ citeproc and open in default odt viewer
-PandocRegisterExecutor PandocOdtBibOpen <LocalLeader>odtb 1 pandoc -t -odt --bibliography g:pandoc_bibfile
-
+PandocRegisterExecutor PandocOdtOpen <LocalLeader>odt 1 pandoc -t odt --bibliography g:pandoc_bibfile
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 " 2. Folding
@@ -298,6 +302,8 @@ function! Pandoc_Find_Bibfile()
 				if filereadable(bib_path . "." . bib_extension)
 					let g:pandoc_bibfile = bib_path . "." . bib_extension
 					let g:pandoc_bibtype = bib_extension
+				else
+					let g:pandoc_bibfile = ""
 				endif
 			endfor
 		endfor

commit 6a8b9a7e6ef2c7f7111ddfea18e9a11e72b4ef72

5e4c4c6 broke bibfile detection. This fixes it.
The Pandoc_Find_Bibfile function actually works in reverse: it finds
every possible match, with the most preferred being the last one it
finds. The change made in 5e4c4c6 meant that it would set g:pandoc_bibfile
to the empty string if it failed to find its most preferred bibfile.

Now it starts by setting g:pandoc_bibfile to the empty string, and then
looks for something better.

plugin/pandoc.vim
@@ -298,13 +298,12 @@ function! Pandoc_Find_Bibfile()
 			let bib_paths = [ local_texmf . g:paths_sep . 'default' ] + bib_paths
 		endif
 		" Now search for the file!
+		let g:pandoc_bibfile = ""
 		for bib_path in bib_paths
 			for bib_extension in bib_extensions
 				if filereadable(bib_path . "." . bib_extension)
 					let g:pandoc_bibfile = bib_path . "." . bib_extension
 					let g:pandoc_bibtype = bib_extension
-				else
-					let g:pandoc_bibfile = ""
 				endif
 			endfor
 		endfor